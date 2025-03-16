2ae61c3e323335a847352ab220321e544dcec57b
CyprianESPI <cyprianespinoza@gmail.com>
2025-03-16 06:46:45 +0100

    App.js link to db

--- app.js
+++ app.js
@@ -7,14 +7,29 @@ const PORT = 3000;
 // Set the path to the directory where your HTML files reside
 const publicDirectoryPath = path.join(__dirname, 'public_pages');
 
+const db = require("../db/queries");
+
+async function getUsernames(req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map(user => user.username).join(", "));
+}
+
+async function createUsernameGet(req, res) {
+  // render the form
+}
+
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
+}
+
 // Serve static files from the public directory
 app.use(express.static(publicDirectoryPath));
 
 // Routes for different HTML pages
-app.get('/', (req, res) => {
-    console.log("usernames will be logged here - wip");
-    res.sendFile(path.join(publicDirectoryPath, 'index.html'));
-});
+app.get('/', getUsernames);
 
 app.get('/new', (req, res) => {
     res.sendFile(path.join(publicDirectoryPath, 'new.html'));
